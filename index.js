1373fab5b4c684ced96f7f4d589ba610ce6bc233
Ryan Punwasi <ryan.punwasi@gmail.com>
2024-04-05 18:38:16 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import { prisma } from "./prisma/prismaInstance.js";
+
+const getUserByEmail = async email => {
+  return await prisma.user.findUnique({ where: { email } });
+};
+
+export const handler = async event => {
+  return await getUserByEmail(process.env.EMAIL);
+};
+
+console.log(await handler());
